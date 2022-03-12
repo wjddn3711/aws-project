fc8c13768cca2e2a67420255d104d5f59b37001e
jung <wjddn3711@naver.com>
2022-03-12 23:28:00 +0900

    aws project

---
+++ src/main/java/com/lee/book/springboot/awsproject/AwsProjectApplication.java
@@ -0,0 +1,13 @@
+package com.lee.book.springboot.awsproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lee/book/springboot/awsproject/domain/posts/PostsRepositoryTest.java
@@ -0,0 +1,48 @@
+package com.lee.book.springboot.awsproject.domain.posts;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+class PostsRepositoryTest {
+
+    @Autowired
+    PostsRepository postsRepository;
+
+    @AfterEach
+    public void cleanUp(){
+        postsRepository.deleteAll();
+    }
+
+    @DisplayName("임의의 게시글을 저장한 후 불러온 게시글과 저장한 정보가 동일한지 확인")
+    @Test
+    void givenPosts_whenFindall_thenGetPosts(){
+        // given
+        String title = "테스트 게시글";
+        String content = "테스트 본문";
+
+        postsRepository.save(Posts.builder()
+                .author("wjddn3711@naver.com")
+                .content(content)
+                .title(title)
+                .build());
+        // When & Then
+        List<Posts> postsList = postsRepository.findAll();
+
+        Posts posts = postsList.get(0);
+        assertThat(posts.getTitle()).isEqualTo(title);
+        assertThat(posts.getContent()).isEqualTo(content);
+
+    }
+
+}
