ef9bff19a98a21e0bb2cc889a7c47598da6e85d1
jung <wjddn3711@naver.com>
2022-03-18 23:21:58 +0900

    IndexController.java ("/") 변경 - Model 객체를 통해 서비스에서 불러온 posts 들을 리스트로 반환

--- src/main/java/com/lee/book/springboot/awsproject/web/IndexController.java
+++ src/main/java/com/lee/book/springboot/awsproject/web/IndexController.java
@@ -1,14 +1,22 @@
 package com.lee.book.springboot.awsproject.web;
 
+import com.lee.book.springboot.awsproject.service.posts.PostsService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequiredArgsConstructor
 @Controller
 public class IndexController {
 
+    private final PostsService postsService;
+
     @GetMapping("/")
-    public String index(){
+    public String index(Model model){
+        model.addAttribute("posts",postsService.findAllDesc());
         return "index";
     }
 
@@ -16,4 +24,4 @@ public class IndexController {
     public String postsSave(){
         return "posts-save";
     }
-}
+}다
