09620b8e0bacc826ff7a341f1427f8df7770bd89
jung <wjddn3711@naver.com>
2022-04-17 20:12:50 +0900

    어노테이션 기반 개선 스프링에서 인식될 수 있도록 WebMvcConfigurer 에 추가합니다

---
+++ src/main/java/com/lee/book/springboot/awsproject/config/WebConfig.java
@@ -0,0 +1,20 @@
+package com.lee.book.springboot.awsproject.config;
+
+import com.lee.book.springboot.awsproject.config.auth.LoginUserArgumentResolver;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    private final LoginUserArgumentResolver loginUserArgumentResolver;
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(loginUserArgumentResolver);
+    }
+}
