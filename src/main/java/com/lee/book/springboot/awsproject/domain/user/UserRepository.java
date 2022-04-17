a032133c1ad19ee670d08b4d63a94b7f2106e562
jung <wjddn3711@naver.com>
2022-04-17 12:19:23 +0900

    UserRepository JPA 를 상속하여 유저의 CRUD 를 담당할 인터페이스 생성

---
+++ src/main/java/com/lee/book/springboot/awsproject/domain/user/UserRepository.java
@@ -0,0 +1,9 @@
+package com.lee.book.springboot.awsproject.domain.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+    Optional<User> findByEmail(String email);
+}
